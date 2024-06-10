351638e1cee9f7a9a5b6d5f74ddbee8fd23063e9
JunHyeok Lee <ljh102715@naver.com>
2024-06-10 11:27:19 +0900

    mypage 기능 구현

--- src/pages/mainPage/leftComponents/leaderBoard.js
+++ src/pages/mainPage/leftComponents/leaderBoard.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// const SERVER_URL = "https://barmi-server.onrender.com";
-// const SOCKET_URL = "wss://barmi-server.onrender.com";
-const SERVER_URL = "http://localhost:8080";
-const SOCKET_URL = "ws://localhost:8080";
+const SERVER_URL = "https://barmi-server.onrender.com";
+const SOCKET_URL = "wss://barmi-server.onrender.com";
+// const SERVER_URL = "http://localhost:8080";
+// const SOCKET_URL = "ws://localhost:8080";
 
 const ONE_MINUTE = 60000;
 
